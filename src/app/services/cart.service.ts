736f169e85a73895a338f401a47ba588913464ef
Tharindu Manoj Thilakarathna <thilakarathnawatm.21@uom.lk>
2024-11-30 19:00:58 +0530

    Create cart service

---
+++ src/app/services/cart.service.ts
@@ -0,0 +1,13 @@
+import { Product } from './../models/product.models';
+import { Injectable, signal } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class CartService {
+  cart = signal<Product[]>([]);
+
+  addToCart(product: Product) {
+    this.cart.set([...this.cart(), product]);
+  }
+}
