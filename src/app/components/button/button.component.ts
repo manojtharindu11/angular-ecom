c992eb7916b4f6a813a357a3a3dda785d6f4da80
Tharindu Manoj Thilakarathna <thilakarathnawatm.21@uom.lk>
2024-11-30 20:17:05 +0530

    Create new button component

---
+++ src/app/components/button/button.component.ts
@@ -0,0 +1,17 @@
+import { Component, input, output } from '@angular/core';
+
+@Component({
+  selector: 'app-button',
+  imports: [],
+  template: `<button
+    class="text-black px-5 py-2 rounded-xl shadow-md hover:bg-slate-50"
+    (click)="buttonClicked.emit()"
+  >
+    {{ label() }}
+  </button>`,
+  styles: ``,
+})
+export class ButtonComponent {
+  label = input();
+  buttonClicked = output();
+}
