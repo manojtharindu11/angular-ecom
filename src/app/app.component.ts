ef6f88e1306d2ba1cb773e86ec36ad08b9bd1a6e
Tharindu Manoj Thilakarathna <thilakarathnawatm.21@uom.lk>
2024-11-30 16:26:58 +0530

    Create primary button component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,11 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './components/header/header.component';
+import { ProductListComponent } from './pages/product-list/product-list.component';
 
 @Component({
   selector: 'app-root',
-  imports: [HeaderComponent],
-  template: '<app-header>',
+  imports: [HeaderComponent, ProductListComponent],
+  template: `<app-header /> <app-product-list />`,
   styles: [],
 })
-export class AppComponent {
-}
+export class AppComponent {}
--- src/app/components/primary-button/primary-button.component.ts
+++ src/app/components/primary-button/primary-button.component.ts
@@ -1,15 +1,17 @@
-import { Component, Input } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
 
 @Component({
   selector: 'app-primary-button',
   imports: [],
-  template: `<button class="" (click)="handleButtonClick()">{{ label }}</button>`,
+  template: `<button
+    class="bg-blue-500 text-white w-full border px-5 py-2 rounded-xl shadow-md hover:opacity-90"
+    (click)="buttonClicked.emit()"
+  >
+    {{ label }}
+  </button>`,
   styles: ``,
 })
 export class PrimaryButtonComponent {
   @Input() label: string = '';
-
-  handleButtonClick() {
-    console.log('Button clicked');
-  }
+  @Output() buttonClicked = new EventEmitter<void>();
 }
