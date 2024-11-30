6b4f3a35a27a762507ab49abdb2beb466e035a6a
Tharindu Manoj Thilakarathna <thilakarathnawatm.21@uom.lk>
2024-11-30 20:17:58 +0530

    Add remove item function

--- src/app/services/cart.service.ts
+++ src/app/services/cart.service.ts
@@ -5,9 +5,28 @@ import { Injectable, signal } from '@angular/core';
   providedIn: 'root',
 })
 export class CartService {
-  cart = signal<Product[]>([]);
+  cart = signal<Product[]>([
+    {
+      id: 1,
+      title: 'Wireless Earbuds',
+      image: 'https://picsum.photos/200/300?random=1',
+      price: 29.99,
+      stock: 50,
+    },
+    {
+      id: 2,
+      title: 'Smartphone Stand',
+      image: 'https://picsum.photos/200/300?random=2',
+      price: 12.99,
+      stock: 0,
+    },
+  ]);
 
   addToCart(product: Product) {
-    this.cart().push(product);
+    this.cart.set([...this.cart(), product]);
+  }
+
+  removeFromCart(id: number) {
+    this.cart.set(this.cart().filter((p) => p.id !== id));
   }
 }
