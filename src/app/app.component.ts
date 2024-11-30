bcb4f1f713703879b3c2883255cf0f6de4f59d24
Tharindu Manoj Thilakarathna <thilakarathnawatm.21@uom.lk>
2024-11-30 19:41:29 +0530

    Define some routes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,11 @@
+import { RouterOutlet } from '@angular/router';
 import { Component } from '@angular/core';
 import { HeaderComponent } from './components/header/header.component';
-import { ProductListComponent } from './pages/product-list/product-list.component';
 
 @Component({
   selector: 'app-root',
-  imports: [HeaderComponent, ProductListComponent],
-  template: `<app-header /> <app-product-list />`,
+  imports: [HeaderComponent, RouterOutlet],
+  template: `<app-header /> <router-outlet />`,
   styles: [],
 })
 export class AppComponent {}
--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,17 +1,19 @@
 import { Component, inject, signal } from '@angular/core';
 import { PrimaryButtonComponent } from '../primary-button/primary-button.component';
 import { CartService } from '../../services/cart.service';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-header',
-  imports: [PrimaryButtonComponent],
+  imports: [PrimaryButtonComponent, RouterLink],
   template: `<div
     class="bg-slate-100 px-4 py-3 shadow-md flex justify-between items-center"
   >
-    <span class="text-xl">Store</span>
+    <button routerLink="/" class="text-xl">Store</button>
     <app-primary-button
       [label]="cart() + ' (' + cartService.cart().length + ')'"
       (buttonClicked)="showButtonClicked()"
+      routerLink="/cart"
     />
   </div>`,
   styles: '',
